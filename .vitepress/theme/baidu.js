914b4f460236870452a6d4912faa12ba1078e6aa
1732889554@qq.com <1732889554@qq.com>
2022-12-10 14:23:11 +0800

    feat:百度统计代码

---
+++ .vitepress/theme/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?bd7e7a2890e38bc050dd06016eecb134";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
