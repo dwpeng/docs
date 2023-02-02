36ea92129906e1bc6fdd61449e812649bbc90083
1732889554@qq.com <1732889554@qq.com>
2023-02-02 15:22:02 +0800

    fix：giscus

--- .vitepress/theme/components/Giscus.vue
+++ .vitepress/theme/components/Giscus.vue
@@ -8,7 +8,7 @@ const { isDark } = useData();
     <ClientOnly>
         <div class="comments-container">
             <Giscus id="comments" repo="dwpeng/docs" repoId="R_kgDOIkT__w" category="Q&A"
-                categoryId="DIC_kwDOIkT__84CT8y0" mapping="title" term="欢迎讨论" reactionsEnabled="1" emitMetadata="0"
+                categoryId="DIC_kwDOIkT__84CT8y0" mapping="title" term="欢迎讨论" reactionsEnabled="1" emitMetadata="1"
                 inputPosition="top" :theme="isDark ? 'dark' : 'light'" lang="zh-CN" />
         </div>
     </ClientOnly>
